dbd8c83fabe60c56af18f5bde7b5431e551a6a0a
NguyenLe <bsbnguyen@gmail.com>
2020-03-02 23:56:07 -0800

    update

---
+++ 03Variable.js
@@ -0,0 +1,12 @@
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+// Khai báo các biến sum, sub, mul, div lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
+var x = 5;
+var y = 2;
+var sum = x + y;
+var sub = x - y;
+var mul = x * y;
+var div = x / y;
+console.log(sum);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
